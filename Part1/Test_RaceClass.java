8dadfb3489899e8201388dc3188d84220123fc0e
Muneeb Musharaf <muneebmusharaf@gmail.com>
2024-04-16 16:20:50 +0100

    commit 11: fix on Test_RaceClass

--- Part1/Test_RaceClass.java
+++ Part1/Test_RaceClass.java
@@ -1,16 +1,17 @@
 public class Test_RaceClass
-{    public static void main(String [] args)
 {
-    System.out.println("RACE CLASS TEST");
+    public static void main(String [] args)
+    {
+        System.out.println("RACE CLASS TEST");
 
-    Race race_1 = new Race(10);
-    Horse race_horse1 = new Horse('\u265E',"race_horse1", 0.9);
-    Horse race_horse2 = new Horse('\u265D',"race_horse2", 0.9);
-    Horse race_horse3 = new Horse('\u2654',"race_horse3", 0.9);
-    race_1.addHorse(race_horse1, 1);
-    race_1.addHorse(race_horse2, 2);
-    race_1.addHorse(race_horse3, 3);
+        Race race_1 = new Race(10);
+        Horse race_horse1 = new Horse('\u265E',"race_horse1", 0.9);
+        Horse race_horse2 = new Horse('\u265D',"race_horse2", 0.9);
+        Horse race_horse3 = new Horse('\u2654',"race_horse3", 0.9);
+        race_1.addHorse(race_horse1, 1);
+        race_1.addHorse(race_horse2, 2);
+        race_1.addHorse(race_horse3, 3);
 
-    race_1.startRace();
-}
+        race_1.startRace();
+    }
 }
